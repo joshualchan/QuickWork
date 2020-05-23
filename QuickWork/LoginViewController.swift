325097607771383bac57e7a734580c337952b7fe
Joshua Chan <joshualchan@gmail.com>
2020-05-22 17:25:30 -0700

    Initial commit – created main frame

---
+++ QuickWork/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  QuickWork
+//
+//  Created by Joshua Chan on 5/22/20.
+//  Copyright © 2020 Joshua Chan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
