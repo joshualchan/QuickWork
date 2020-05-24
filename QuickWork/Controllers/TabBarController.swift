f6fddab86455819a66c749fb923d5223669673a3
Joshua Chan <joshualchan@gmail.com>
2020-05-24 15:56:08 -0700

    Created tab bar controller to make the feed page the default when signing in

---
+++ QuickWork/Controllers/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  QuickWork
+//
+//  Created by Joshua Chan on 5/24/20.
+//  Copyright © 2020 Joshua Chan. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 1
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
