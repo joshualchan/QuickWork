32b0a68f805000d9abaf8e9c17d7b84352b46c63
Joshua Chan <joshualchan@gmail.com>
2020-05-27 21:22:14 -0700

    created chat view controller and messages view controller

---
+++ QuickWork/Controllers/ChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatViewController.swift
+//  QuickWork
+//
+//  Created by Joshua Chan on 5/27/20.
+//  Copyright © 2020 Joshua Chan. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+class ChatViewController: MessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
